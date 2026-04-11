f0db6861bf0f06e36cb15862ff1f3e030f5ae560
NLASL <nadallavernia.gestio@gmail.com>
2026-04-11 16:41:47 +0200

    Add files via upload

--- veu.v1.js
+++ veu.v1.js
@@ -113,7 +113,7 @@ async function interpretarVeu(text, treballadorId) {
     let fincaTrobada = null;
     let scoreFinca = 1;
     
-    const paraules = textNorm.split(/[\s,\.]+/).filter(function(p) { return p.length > 2; });
+    const paraules = textNorm.split(/[\s,\.]+/).filter(function(p) { return p.length > 1; });
     
     const fuseFinca = new Fuse(finques, {
         threshold: 0.5,
