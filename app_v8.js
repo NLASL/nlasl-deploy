167097e82e5ac2c0c599c4ea398565dcb92e2d56
NLASL <nadallavernia.gestio@gmail.com>
2026-05-02 01:51:44 +0200

    Add files via upload

--- app_v8.js
+++ app_v8.js
@@ -1161,11 +1161,10 @@ async function carregarVistaFertilitzacions() {
     
     container.innerHTML = html;
 
-    // ✔ Carregar campanyes i aplicar filtre FUNCIÓ DE MOMENT NO NECESSARIA
-    // await carregarCampanyes();
-    //document.getElementById('filtre-campanya').addEventListener('change', carregarTaulaFertilitzacions);
-
-    //await carregarTaulaFertilitzacions();
+	// ✔ Carregar campanyes i aplicar filtre
+	// await carregarCampanyes();  // ← ELIMINA O COMENTA
+	document.getElementById('filtre-campanya').addEventListener('change', carregarTaulaFertilitzacions);
+	await carregarTaulaFertilitzacions();
 }
 
 
