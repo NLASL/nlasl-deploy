13f9283f7229e6172b0bca61327082fef1d0504b
NLASL <nadallavernia.gestio@gmail.com>
2026-05-17 05:19:31 +0200

    Add files via upload

--- collita-ui.v1.js
+++ collita-ui.v1.js
@@ -16,8 +16,7 @@ async function carregarVistaCollita() {
         await mostrarVista_Escandalls();
     } else if (vistaColltitaActual === 'registres') {
         await mostrarVista_Registres();
-    
-	} else if (vistaColltitaActual === 'analisi') {
+    	} else if (vistaColltitaActual === 'analisi') {
 		await mostrarVista_Analisi();
 	}
 }
