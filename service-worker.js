9f742358f2f92dd92571d3b5286d9e63727bb3f2
NLASL <nadallavernia.gestio@gmail.com>
2026-05-21 01:57:26 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 // VERSIÓ - Incrementar aquest número per forçar actualització
-const VERSION = 'v108';
+const VERSION = 'v109';
 const CACHE_NAME = 'quadern-camp-nlasl-' + VERSION;
 
 // Fitxers a cachear
