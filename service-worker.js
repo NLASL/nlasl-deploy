cec03d16dcfab15f360cc16d154163ddc3178fce
NLASL <nadallavernia.gestio@gmail.com>
2026-05-31 02:22:38 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 // VERSIÓ - Incrementar aquest número per forçar actualització
-const VERSION = 'v116';
+const VERSION = 'v117';
 const CACHE_NAME = 'quadern-camp-nlasl-' + VERSION;
 
 // Fitxers a cachear
