5102dc5530e36f7ead004f2173de0de9510fceaa
NLASL <nadallavernia.gestio@gmail.com>
2026-05-31 02:09:44 +0200

    Add files via upload

--- app_v8.js
+++ app_v8.js
@@ -212,7 +212,7 @@ function canviarVista(vista) {
 			carregarVistaControlHorari();
 			}
 			break;
-		    });
+		    };
     if (treballadorActiu && role === 'visor') {
         carregarVistaControlHorariTreballador();
     } else {
